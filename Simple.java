67ee59d689fecd63fdd3d8035593ce7ca16f4b81
Kirill <hammerroofficial@gmail.com>
2025-03-26 11:51:57 +0300

    Update Simple.java
    Variant 1

--- Simple.java
+++ Simple.java
@@ -1,5 +1,10 @@
-class Simple{
-    public static void main(String args[]){
+public class Simple
+{
+    public static void main(String args[])
+    {
         System.out.println("Hello Java");
+        System.out.println("Фамилия: Пальчевский");
+        System.out.println("Имя: Кирилл");
+        System.out.println("Дата: " + new java.util.Date());
     }
 }
